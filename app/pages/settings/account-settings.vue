7534b2fbd7f0c3e59c18a707e3713b8d63812272
unknown <ronnien@mosaic-solutions.com>
2021-11-09 03:06:33 +0800

    settings page

---
+++ app/pages/settings/account-settings.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="dashboard-content">
+      <div class="container-fluid">
+            <div class="row">
+            <div class="col-md-6">
+                <div class="dashboard-title">
+                <h3>Settings</h3>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    layout : 'main'
+}
+</script>
